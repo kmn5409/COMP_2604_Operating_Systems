ff3ff35d412c47b96b0ef645228f81d518c36345
Keanu Nichols <nichols.keanu9@gmail.com>
2019-02-28 10:41:58 -0400

    Add explanation and changed function names
    -So I added an explanation as to how the program runs 
    -I updated the function names because it was different from the names
    given in the assignment

--- Atonia's_solution/golseq.c
+++ Atonia's_solution/golseq.c
@@ -4,19 +4,28 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 
-int golumb(int n) //gets the nth value of the Golumb Sequence
+/*
+The user will first enter a value into the command line. If it is not a positive number for example in these cases of entering a 
+negative number, 0 or a string, it will ask the user to re-enter a number. The positive value will then be converted to an
+int and a fork will be generated. A conditonal statement will then occur whereby the process id is checked, if it is equal to 0
+the printGolumb function is called. This function then iterates through a loop that uses the Golomb function  (which is recursive in nature)
+that generates the nth Golomb number passed as a value. If the pid is more than 0 the parent process the process waits for the child process to finish,
+then prints "Finished". If the pid is negative the program prints "Fork Failed" and the program then finishes.
+*/
+
+int Golomb(int n) //gets the nth value of the Golumb Sequence
 {
    if (n==1)
 return 1;
-   return 1+ golumb( n-golumb(golumb(n-1)) );
+   return 1+ Golomb( n-Golomb(Golomb(n-1)) );
 }
 
 
-void printGolumb(int n) //prints Golumb Sequence up to n
+void printGolomb(int n) //prints Golumb Sequence up to n
 {
   printf("\nGenerating Golomb Sequence...\n");
     for(int i=1;i<=n;i++)
-printf("%d ",golumb(i));
+printf("%d ",Golomb(i));
 }
 
 int main(int argc, char* argv[])
@@ -35,7 +44,7 @@ int main(int argc, char* argv[])
   int status;
 
   if(id==0) //print in child
-printGolumb(num);
+printGolomb(num);
   else if(id>1)  //wait on child
   {
 id=wait(&status);
